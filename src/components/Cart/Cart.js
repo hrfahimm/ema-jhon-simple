4d73e961913b728bf3ea516115ea159d2c71343f
hrfahimm <fahimtaj10@gmail.com>
2023-03-01 12:27:21 +0600

    'hh'

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,24 @@ import React from "react";
 import "./Cart.css";
 const Cart = (props) => {
 	const {cart} = props;
+	// const totalReducer = (previous, product) => previous + product.price;
+	// const total = cart.reduce(totalReducer, 0);
 	let total = 0;
 	for (const product of cart) {
 		total = total + product.price;
 	}
+	const shipping = 15;
+	const tax = (total + shipping) * 10;
+	const grandTotal = total + shipping + tax;
+
 	return (
-		<div>
-			<h3>order</h3>
-			<h5>Item Orders: {props.cart.length}</h5>
-			<p>Total: {total}</p>
+		<div className="cart-order menu-icon">
+			<h3>Order</h3>
+			<h5>Item Orders--: {props.cart.length}</h5>
+			<p>Total---------: {total.toFixed(2)}</p>
+			<p>Shipping----: {shipping}</p>
+			<p>Tax-----------: {tax.toFixed(2)}</p>
+			<h4 classsName="g-total">Grand Total-: {grandTotal.toFixed(2)}</h4>
 		</div>
 	);
 };
